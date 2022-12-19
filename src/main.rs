0200e32c2e725d968ce2b9912c5d47fbea631cff
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2022-12-20 01:08:25 +1100

    added random num

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -6,12 +7,24 @@ fn main() {
     let secret_num = rand::thread_rng().gen_range(1, 101);
     println!("Enter your sec num {}", secret_num);
 
-    println!("Enter your guess: ");
+    loop {
+        println!("Enter your guess: ");
 
-    let mut guess = String::new(); // by default variables are immutable in rust. Use the keyword mut to make it mutable
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to readline");
+        let mut guess = String::new(); // by default variables are immutable in rust. Use the keyword mut to make it mutable
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");
 
-    println!("you guessed: {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+        println!("you guessed: {}", guess);
+        match guess.cmp(&secret_num) {
+            Ordering::Equal => {
+                println!("You win ");
+                break;
+            }
+            Ordering::Greater => println!("Too big"),
+            Ordering::Less => println!("Too small"),
+        }
+    }
 }
