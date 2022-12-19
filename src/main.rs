4b2a97baf92c0e0ef44beb517896bb1393a0e215
Vigneishwar <36326070+vigneishwar@users.noreply.github.com>
2022-12-20 00:54:13 +1100

    added random num

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the num!");
+    let secret_num = rand::thread_rng().gen_range(1, 101);
+    println!("Enter your sec num {}", secret_num);
 
     println!("Enter your guess: ");
 
